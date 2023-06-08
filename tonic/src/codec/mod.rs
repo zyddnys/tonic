d8c428ce2fb570c0459e1a42e9b4ee3529f4fb19
zyddnys <zyddnys@outlook.com>
2023-06-08 18:10:46 -0400

    Update mod.rs

--- tonic/src/codec/mod.rs
+++ tonic/src/codec/mod.rs
@@ -29,8 +29,8 @@ const HEADER_SIZE: usize =
     // data length
     std::mem::size_of::<u32>();
 
-// The default maximum uncompressed size in bytes for a message. Defaults to 4MB.
-const DEFAULT_MAX_RECV_MESSAGE_SIZE: usize = 4 * 1024 * 1024;
+// The default maximum uncompressed size in bytes for a message. Defaults to 400MB.
+const DEFAULT_MAX_RECV_MESSAGE_SIZE: usize = 400 * 1024 * 1024;
 const DEFAULT_MAX_SEND_MESSAGE_SIZE: usize = usize::MAX;
 
 /// Trait that knows how to encode and decode gRPC messages.
